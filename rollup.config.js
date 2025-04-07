b646365588687c8be93d4ce6c2698167c2827e3b
Jatin Thakur <73630627+JatinThakur-12@users.noreply.github.com>
2025-04-07 22:44:32 +0530

    add rollup config with plugins

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import packageJson from "./package.json";
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import { dts } from 'rollup-plugin-dts';
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourceMap: true,
+            },
+            {
+                file: packageJson.main,
+                format: "esm",
+                sourceMap: true,
+            }
+        ],
+        plugins: [
+            peerDepsExternal(),
+            nodeResolve(),
+            commonjs(),
+            typescript({
+                tsconfig: "./tsconfig.json",
+            }),
+            terser()
+        ],
+        external: ['react', 'react-dom']
+    },
+    // for typescript types files 
+    {
+        input: 'src/index.ts',
+        output: {
+            file: packageJson.types
+        }, plugins: [
+            //NOTE: check it before finalizing with dts.default()
+            dts(),
+        ]
+    }
+];
