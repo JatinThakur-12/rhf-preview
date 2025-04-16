5ce375d99e6380d887e59ff7b21bed1519891762
Jatin Thakur <73630627+JatinThakur-12@users.noreply.github.com>
2025-04-16 23:58:40 +0530

    portal with trigger

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { useFormContext } from "react-hook-form";
+import Preview from "./preview";
 
 const RHFPreview = () => {
     const { watch, formState } = useFormContext();
+    const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
     const values = watch();
-    return <div>Running RHF Success</div>;
+    return (
+        <>
+            <div>Running RHF check</div>
+            {createPortal(
+                <div style={{ position: 'absolute', bottom: '0', left: '0' }}><button onClick={() => { setIsDrawerOpen(prev => !prev) }}>
+                    Open Drawer
+                </button>
+                </div >,
+                document.body
+            )
+            }
+            {/* {createPortal("Hi Jatin", document.body)} */}
+            {isDrawerOpen && createPortal(<Preview />, document.body)}
+        </>
+    );
 };
 
 export default RHFPreview;
---
+++ src/preview.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Preview = () => {
+    return (
+        <div style={{ position: 'absolute', top: '0', right: '0' }}>Preview</div>
+    )
+}
+
+export default Preview
