dde6077e6f46677e655ed0d39a5f32ad03b6467c
Jatin Thakur <73630627+JatinThakur-12@users.noreply.github.com>
2025-04-22 22:28:29 +0530

    filter and sorted form state

--- src/components/form-state.tsx
+++ src/components/form-state.tsx
@@ -1,12 +1,16 @@
 import { useFormContext } from 'react-hook-form';
 
+const EXCLUDED_STATE = ['errors', 'validatingFields', 'touchedFields', 'dirtyFields', 'defaultValues'];
 function FormState() {
     const { formState } = useFormContext();
     return (
         <>
             <div>FormState</div>
-            {Object.getOwnPropertyNames(formState).map((key) => {
+            {Object.getOwnPropertyNames(formState).sort().map((key) => {
                 const typedKey = key as keyof typeof formState;
+                if (EXCLUDED_STATE.includes(key)) {
+                    return null;
+                }
                 return (<div key={key} style={{ display: 'flex', flexDirection: 'row', gap: '4px' }}>
                     <span>{JSON.stringify(key)}</span>
                     <span>:</span>
